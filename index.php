86ecccfcc2d513a44e7b38790d576ebb5e361ebb
okzapradhana <okzamahendra29@gmail.com>
2018-08-12 17:43:07 +0700

    greeting, join messages are available, and also /weather following by city name

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-require('./vendor/autoload.php');
 require_once './lineBot.php';
 
 
@@ -9,33 +7,32 @@ $text = $bot->getMessageText();
 $eventType = $bot->getEventType();
 $messageType = $bot->getMessageType();
 // $bot->reply($text);
+$textExplodeWeather = explode(' ', $text);
+$userDisplayName = $bot->getUserName($bot->getUserId());
 
-if($eventType == "join"){
+if ($eventType == "join"){
   $bot->replyUsingText("Terimakasih telah mengundang saya kesini bos! Ada yang bisa dibantu?");
-} else if($eventType == "message"){
+} else if ($eventType == "message"){
     if($messageType == "text"){
       if($text == "/leave"){
         if($bot->getRoomChatType() == "group"){
           $bot->leaveGroup($bot->getGroupId());    
         }
       } else if($text == "/groupId"){
-        $bot->getGroupId();
+        $bot->printGroupId();
+      } else if($textExplodeWeather[0] == "/weather"){
+        $bot->getWeatherBasedOnCityName($textExplodeWeather[1]);
+      } else if(strpos($text, "makasih") || strpos($text, "thx")){
+        $bot->replyUsingText("Siap! sama-sama " . $userDisplayName);
       } else{
         $bot->replyUsingText('Maaf, bisa diulangi bos? Saya masih belajar :(');
       }
     }
+} else if ($eventType == "follow"){
+    $bot->replyUsingText("Helo! " . $userDisplayName . " kenalin saya With Her. Bot yang bakal bantu kamu untuk tau info cuaca. Keyword yang bisa kamu pakai yaitu: \n
+    - /weather {nama_kota} untuk cek cuaca berdasarkan nama kota
+    - /leave untuk keluar dari grup, ataupun room chat. Selamat mencoba!
+    ");
 }
-
-// switch($text){
-//   case '/leave':
-//     $bot->leaveGroup();
-//     break;
-//   case '/groupId':
-//     $bot->getGroupId();
-//     break;
-//   default:
-//     $bot->replyUsingText('Maaf, bisa diulangi bos?');
-//     break;
-// }
 
 
